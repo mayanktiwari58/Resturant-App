9ae726c99514cc043ecda2603cafbe60bfd7d491
mayanktiwari58 <76210281+mayanktiwari58@users.noreply.github.com>
2022-08-22 11:18:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Resturant from "./component/Basics/Resturant";
+// import UseState from "./component/Hooks/useState";
+// import UseEffect from "./component/Hooks/useEffect";
+
+
+const App = () => {
+  return <Resturant/>;
+};
+
+export default App;
---
+++ src/component/Basics/menuApi.js
@@ -0,0 +1,86 @@
+const Menu = [
+    {
+      id: 1,
+      image: "https://st.depositphotos.com/1005893/4244/i/450/depositphotos_42443153-stock-photo-asian-spicy-fried-curry-instant.jpg",
+      name: "maggi",
+      category: "breakfast",
+      price: "12₹",
+      description:
+        "I love Maggi realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, eius necessitatibus fugiat in . ",
+    },
+  
+    {
+      id: 2,
+      image: "https://media.istockphoto.com/photos/indian-tea-time-snacks-in-group-picture-id1156020805?k=20&m=1156020805&s=612x612&w=0&h=j2pHo5Kyd20bdcI-v_YRFdKLoGuy0MmOWNEIsGvAYmQ=",
+      name: "allu pakoida",
+      category: "evening",
+      price: "20₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 3,
+      image: "https://thumbs.dreamstime.com/b/sweet-fresh-corn-could-be-harvested-55713290.jpg",
+      name: "corn",
+      category: "breakfast",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 4,
+      image: "https://image.shutterstock.com/image-photo/indian-dish-spicy-chick-peas-260nw-1705528756.jpg",
+      name: "chola",
+      category: "lunch",
+      price: "50₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 5,
+      image: "https://thumbs.dreamstime.com/b/pizza-12182650.jpg",
+      name: "pizza",
+      category: "evening",
+      price: "80₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 6,
+      image: "https://thumbs.dreamstime.com/b/indian-thali-26440151.jpg",
+      name: "Non-Veg Thali",
+      category: "dinner",
+      price: "180₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 7,
+      image: "https://thumbs.dreamstime.com/b/indian-sweets-21144108.jpg",
+      name: "Sweets",
+      category: "dinner",
+      price: "60₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 8,
+      image: "https://media.istockphoto.com/photos/kidney-bean-curry-or-rajma-or-rajmah-chawal-and-roti-typical-north-picture-id669635320?k=20&m=669635320&s=612x612&w=0&h=bf3AZPXWlxquOIJx-dHfnV5HGq_bjhncBROZZa2zQwE=",
+      name: "Rajma Rice",
+      category: "lunch",
+      price: "60₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 9,
+      image: "https://wallpaperaccess.com/full/2069188.jpg",
+      name: "samaso",
+      category: "evening",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+  ];
+  
+  export default Menu;
